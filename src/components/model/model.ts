6ef56a6df03a4d1601dd53fc3835975752748b85
Степан <rainbowprodder@gmail.com>
2017-10-08 05:59:59 +0700

    Реализация аксессоров

--- src/components/model/model.ts
+++ src/components/model/model.ts
@@ -1,6 +1,66 @@
 import Event from './../observer/observer';
 import { IModel } from './namespace';
 
-export default class Model{
-  
+export default class Model implements IModel {
+  public fieldChanged: Event;
+
+  private _field: Array<Array<boolean>>;
+  private _minWidth = 10;
+  private _minHeight = 10;
+
+  public constructor(private _width = 50, private _height = 50) {
+    this.width = this._width;
+    this.height = this._height;
+  }
+  // accessors
+  set height(value: number) {
+    if (this._width < this._minWidth) {
+      this._width = 50;
+    } else {
+      this._width = Math.floor(this._width);
+    }
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  set width(value: number) {
+    if (this._height < this._minHeight) {
+      this._height = 50
+    } else {
+      this._height = Math.floor(this._height);
+    }
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get field() {
+    return this._field;
+  }
+
+  // public methods
+  public nextGeneragion() {
+
+  }
+
+  public toggleCell(cellX: number, cellY: number) {
+
+  }
+
+  public clear() {
+
+  }
+
+  public import(file: JSON) {
+
+  }
+
+  public export() {
+
+  }
+
+  // private methods
 }
--- src/components/model/namespace.ts
+++ src/components/model/namespace.ts
@@ -1,9 +1,8 @@
 import Event from './../observer/observer';
 
 export interface IModel {
-  readonly itemAdded: Event;
-  readonly itemRemoved: Event;
-  readonly selectedIndexchanged: Event;
-  readonly items: Array<string>;
-  readonly selectedIndex: number;
+  readonly width: number;
+  readonly height: number;
+  readonly field: Array<Array<boolean>>;
+  readonly fieldChanged: Event;
 }
